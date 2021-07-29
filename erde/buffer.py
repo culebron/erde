b6cc13b12c9b7b097111006738704fbaab455966
Dmitri Lebedev <ryba4@ryba4.com>
2021-07-29 23:38:09 +0700

    buffer.py 100% coverage

--- erde/buffer.py
+++ erde/buffer.py
@@ -1,16 +1,34 @@
 import geopandas as gpd
 import numpy as np
 
-def buffer(data, radius, *args, **kwargs):
+def buffer(data, radius, dissolve=False, default_crs=None, *args, **kwargs):
 	"""
 	Creates buffer as in shapely.buffer
 	https://shapely.readthedocs.io/en/stable/manual.html#object.buffer
+
+	Parameters
+	----------
+
+	* `data` GeoSeries or GeoDataFrame, the geometries to make buffer of.
+	* `radius` float, radius in metres.
+	* `dissolve`: bool, default `False`
+		unite overlapping geometries. Index will be lost. If `data` is a GeoDataFrame, will return a new GeoDataFrame with new index and no other columns.
+	* `resolution`: int, default 10. Number of vertice in a 90° arc.
+
+	Shapely-specific parameters (see Shapely doc for more info):
+
+	* `cap_style`: int (0, 1, or 2). Style of buffers at ends of lines.
+	* `join_style`: int (0, 1, or 2), style of buffer around corners.
+	* `mitre_limit`: float (1..5) how far a sharp join should protrude.
 	"""
 	if not isinstance(data, (gpd.GeoSeries, gpd.GeoDataFrame)):
 		raise TypeError('geodata must be GeoSeries/GeoDataFrame')
 
 	if data.crs is None:
-		data.crs = 4326
+		if default_crs is None:
+			raise ValueError(f'data ({data.__class__}) has no crs. Set the CRS of the GeoSeries/GeoDataFrame manually, or call with `default_crs=4326` (or any appropriate) to set default behavior.')  # This exception is raised because setting CRS 4326 silently would cause unexpected behavior and exceptions in other locations. Crashing earlier is better.
+
+		data.crs = default_crs
 
 	old_crs = data.crs
 	series = data if isinstance(data, gpd.GeoSeries) else data[data._geometry_column_name]
@@ -19,7 +37,8 @@ def buffer(data, radius, *args, **kwargs):
 	buf = series.buffer(radius / series.centroid.to_crs(4326).y.pipe(np.radians).pipe(np.cos), *args, **kwargs).to_crs(old_crs)
 
 	if isinstance(data, gpd.GeoSeries):
-		return buf
+		return gpd.GeoSeries(buf.unary_union, crs=old_crs) if dissolve else buf
 
+	data = data.copy()
 	data[data._geometry_column_name] = buf
-	return data
+	return gpd.GeoDataFrame({data._geometry_column_name: buf.unary_union}, crs=old_crs) if dissolve else data
--- tests/buffer/test_buffer.py
+++ tests/buffer/test_buffer.py
@@ -1,12 +1,58 @@
+import pytest
+import geopandas as gpd
+from geopandas.testing import assert_geoseries_equal
 from erde import buffer, read_df
 
-df = read_df('tests/buffer/points.geojson')
+points_df = read_df('tests/buffer/points.geojson')
+bufs_df = read_df('tests/buffer/buffers.geojson')
+bufs_dissolved_df = read_df('tests/buffer/buffers-dissolved.geojson')
 
 def test_geoseries():
+	assert len(points_df) == 7
 	# if we call buffer with geoseries, it returns geoseries
 
+	init_gtype = points_df.geometry.geom_type
+	res = 5
+	bufs2_df = buffer.buffer(points_df, 500, resolution=res)
 
+	# check that resolution works
+	assert len(bufs2_df.geometry.values[0].exterior.coords) == res * 4 + 1
 
-# geoseries vs geodataframe
-# crs vs crs is none
-# resolution default vs 3 vs 5
+	# original dataframe & geometries not changed
+	assert all(points_df.geometry.geom_type == init_gtype)
+
+	# geodataframe => geodataframe
+	assert isinstance(bufs2_df, gpd.GeoDataFrame)
+	assert_geoseries_equal(bufs_df.geometry, bufs2_df.geometry, check_less_precise=True)
+	assert len(points_df) == len(bufs2_df)  # same number of geometries (not so with dissolve)
+	assert all(points_df.index == bufs2_df.index)  # same indice
+	assert bufs2_df.crs == points_df.crs  # same CRS
+
+	# if geoseries, must return geoseries
+	bufs_series = buffer.buffer(points_df.geometry, 500, resolution=res)
+	assert isinstance(bufs_series, gpd.GeoSeries)
+	assert all(bufs_series.index == points_df.index)  # same index
+
+	res = 10
+	bufs3_df = buffer.buffer(points_df, 500, resolution=res)
+	# all geometries must be different if resolution is different
+	assert not any(bufs3_df.geometry.geom_almost_equals(bufs_df.geometry))
+	# check resolution has effect
+	assert len(bufs3_df.geometry.values[0].exterior.coords) == res * 4 + 1
+
+	bufs4_df = buffer.buffer(points_df, 500, resolution=5, dissolve=True)
+	assert len(bufs4_df) == 4
+
+	# default crs
+	p2 = points_df.copy()
+	p2.crs = None
+
+	with pytest.raises(ValueError):
+		buffer.buffer(p2, 500)
+
+	bufs5_df = buffer.buffer(p2, 500, resolution=5, default_crs=4326)
+	assert_geoseries_equal(bufs5_df.geometry, bufs_df.geometry, check_less_precise=True)
+
+	# must raise TypeError if data is not geoseries or geodataframe
+	with pytest.raises(TypeError):
+		buffer.buffer(points_df.geometry.values[0], 500)
