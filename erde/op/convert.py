43a4a4560a97034b5e02af81dbaa284a166d7b84
Dmitri Lebedev <ryba4@ryba4.com>
2021-08-03 13:51:27 +0700

    type conversion & checking in convert

--- erde/op/convert.py
+++ erde/op/convert.py
@@ -1,4 +1,12 @@
 from erde import autocli, read_stream, write_stream
+import yaargh
+
+
+def toint(val, name):
+	try:
+		return int(val)
+	except ValueError:
+		raise yaargh.CommandError(f'{name} must be int')
 
 
 @autocli
@@ -7,12 +15,12 @@ def main(path1, path2, to_crs=None, from_crs=None):
 	with read_stream(path1, sync=False) as rd, write_stream(path2, sync=False) as wr:
 		for df in rd:
 			if from_crs is not None:
-				df.crs = from_crs
+				df.crs = toint(from_crs, 'from_crs')
 
 			if to_crs is not None:
 				if df.crs is None:
 					raise RuntimeError(f'Input GeoDataFrame has no CRS, but convesion is requested with to_crs={to_crs}')
 
-				df.to_crs(to_crs, inplace=True)
+				df.to_crs(toint(to_crs, 'to_crs'), inplace=True)
 
 			wr(df)
