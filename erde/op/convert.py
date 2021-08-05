323bc3f8270a3902825d8f2aa62c90f3ba1f16c1
Dmitri Lebedev <ryba4@ryba4.com>
2021-08-05 08:24:38 +0700

    removed debug lines

--- erde/op/convert.py
+++ erde/op/convert.py
@@ -4,9 +4,6 @@ from erde import read_stream, write_stream, autocli
 @autocli
 def main(df: read_stream, to_crs:int=None, from_crs:int=None) -> write_stream:
 	"""Converts files/tables between formats and CRS."""
-	print('df', df)
-	print('to_crs', to_crs)
-	print('from_crs', from_crs)
 	if from_crs is not None:
 		df.crs = from_crs
 
