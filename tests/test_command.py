495258b9ef0d69858c7ecb599ff437db0e5b093a
Dmitri Lebedev <ryba4@ryba4.com>
2021-08-06 15:37:47 +0700

    refactored test of command

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from erde import autocli, read_df
+from erde import autocli, read_df, read_stream, write_stream
 from unittest import mock
 import geopandas as gpd
 
@@ -25,36 +25,35 @@ def test_cli_call():
 	def crashing_func(input_data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 		raise ValueError("let's crash")
 
+	def streaming_func(input_data: read_stream) -> write_stream:
+		return input_data[:10]
+
 	# pretend we're decorating in __main__
 	m1 = mock.MagicMock()
 	m1.return_value.__name__='__main__'
-	with mock.patch('inspect.getmodule', m1), mock.patch('yaargh.dispatch', mock.MagicMock()):
+
+	m4 = mock.MagicMock()
+	m4.return_value.__getitem__.return_value.__getitem__.return_value = '/tmp/test-output-file.gpkg'
+	with mock.patch('inspect.getmodule', m1), mock.patch('yaargh.dispatch', mock.MagicMock()), mock.patch('yaargh.ArghParser.parse_known_args', new=m4):
 		clifunc3 = autocli(clifunc)
 		crashing_func2 = autocli(crashing_func)
 
-		m4 = mock.MagicMock()
-		m4.return_value.__getitem__.return_value.__getitem__.return_value = '/tmp/test-output-file.gpkg'
-		with mock.patch('yaargh.ArghParser.parse_known_args', new=m4):
-			def run_with_patches(ipdb=0, pudb=0):
-				with mock.patch('erde.IPDB', ipdb), mock.patch('erde.PUDB', pudb), mock.patch('ipdb.slaunch_ipdb_on_exception', mock.MagicMock()) as mipdb, mock.patch('erde._handle_pudb', mock.MagicMock()) as mpudb:
-					clifunc3(df)
+		# tests if debuggers were initialized (but no exception is raised here)
+		for ipdb, pudb in ((0, 0), (1, 0), (0, 1)):
+			with mock.patch('erde.IPDB', ipdb), mock.patch('erde.PUDB', pudb), mock.patch('ipdb.slaunch_ipdb_on_exception', mock.MagicMock()) as mipdb, mock.patch('erde._handle_pudb', mock.MagicMock()) as mpudb:
+				clifunc3(df)
 
-				return mipdb, mpudb
+			for param, obj in ((ipdb, mipdb), (pudb, mpudb)):
+				attr = 'assert_not_called' if param == 0 else 'assert_called_once'
+				getattr(obj, attr)()
 
-			mi, mp = run_with_patches(0, 0)
-			mi.assert_not_called()
-			mp.assert_not_called()
+			return mipdb, mpudb
 
-			mi, mp = run_with_patches(1, 0)
-			mi.assert_called_once()
-			mp.assert_not_called()
+		# make sure debugger is called when an exception is raised
+		with mock.patch('erde.PUDB', 1), mock.patch('pudb.post_mortem', mock.MagicMock()) as pm_debug:
+			crashing_func2(df)
 
-			mi, mp = run_with_patches(0, 1)
-			mi.assert_not_called()
-			mp.assert_called_once()
-
-			with mock.patch('erde.PUDB', 1), mock.patch('pudb.post_mortem', mock.MagicMock()):
-				crashing_func2(df)
+		pm_debug.assert_called_once()
 
 	assert not hasattr(clifunc3, '_argh')
 	assert clifunc3 != clifunc
