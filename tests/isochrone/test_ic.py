a5beb79011f4511db2f8213139902e7fbecb3dd7
Dmitri Lebedev <ryba4@ryba4.com>
2021-08-28 01:40:39 +0700

    +test of setting routed df

--- tests/isochrone/test_ic.py
+++ tests/isochrone/test_ic.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from erde import read_df, read_geom
 from erde.op import isochrone as ic
 from shapely.geometry import box, Point
@@ -29,25 +30,29 @@ def test_grid():
 	# check that the new grid is roughly double of the first one
 	assert abs((grid_size2 - grid_size * 2) / grid_size / 2) < .1
 
+def _get_pickled_table():
+	import pickle
+
+	# dict with records was saved to pickle, because otherwise python3.6 crashes when trying to recreate a dumped DataFrame
+	with open(t + 'table.pickle', 'rb') as f:
+		return pd.DataFrame(pickle.load(f))
 
 def test_polygons():
 	# test if with given table, the result is about the same
 	ir = get_ir()
 
 	polys = read_df(t + 'polygons.csv').set_index('duration')
+	polys.crs = 4326
+	polys.to_crs(3857, inplace=True)
 
-	import pickle
-
-	# dict with records was saved to pickle, because otherwise python3.6 crashes when trying to recreate a dumped DataFrame
-	with open(t + 'table.pickle', 'rb') as f:
-		df = pd.DataFrame(pickle.load(f))
+	df = _get_pickled_table()
 
 	with mock.patch('erde.op.isochrone.table_route', return_value=[df]):
-		for i, r in ir.polygons.iterrows():
+		for i, r in ir.polygons.to_crs(3857).iterrows():
 			other = polys.loc[r.duration, 'geometry']
-			print(r['geometry'])
-			print(other)
-			assert r['geometry'].almost_equals(other)
+			# geom_almost_equals sometimes does not work, but difference is in micrometers. To fix it, let's expand polygons by 10 cm and check if the other is inside. If difference is any bigger, one will stick out.
+			assert r.geometry.within(other.buffer(.1, resolution=3))
+			assert other.within(r.geometry.buffer(.1, resolution=3))
 
 
 class PropertyMock(mock.Mock):
@@ -82,3 +87,29 @@ def test_set_grid():
 
 	m.assert_called_once()
 	assert ir._grid is None
+
+
+@contextmanager
+def _patch_table(df):
+	with mock.patch('erde.op.isochrone.table_route', return_value=[df]) as patched_route:
+		yield patched_route
+
+
+def test_set_routed():
+	ir = get_ir()
+	new_routed = read_df(t + 'routed.csv')[-10:]
+
+	df = _get_pickled_table()
+	with _patch_table(df) as patched_table:
+		assert len(ir.routed) == len(df) + 1
+		assert len(new_routed) != len(ir.routed)
+		assert ir._routed is not None
+		patched_table.assert_called_once()
+
+	ir = get_ir()
+	with _patch_table(df) as patched_table:
+		assert ir._routed is None
+		ir.routed = new_routed
+		assert ir._routed is not None
+		assert len(ir.routed) == len(new_routed)
+		patched_table.assert_not_called()
